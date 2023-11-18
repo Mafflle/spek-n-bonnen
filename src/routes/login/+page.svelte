99201233acbb52cd10435bbcce317d89b19d7b26
n3rd <godwinjdayo@gmail.com>
2023-11-18 22:56:04 +0100

    login form

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+	import { Card, Label, Input } from 'flowbite-svelte';
+</script>
+
+<div class="h-screen w-screen flex justify-center items-center bg-[#F5F5F5]">
+	<form>
+		<Card
+			class="bg-white shadow-none rounded-[16px] py-[50px] px-[30px] text-[#2d2d2d] flex gap-10"
+		>
+			<div class="greeting">
+				<div class="flex gap-[1.88rem] items-center w-full">
+					<div class="avatar w-1/5">
+						<img src="/icons/user.svg" alt="avatar" />
+					</div>
+					<div class="admin flex flex-col gap-2 w-auto">
+						<h1 class="text-2xl font-medium tracking-[ -0.03rem] whitespace-nowrap">
+							Welcome back admin
+						</h1>
+						<sub class="text-[0.9375rem] tracking-[-0.00938rem] text-[#575757]"
+							>Enter your credentials to log in</sub
+						>
+					</div>
+				</div>
+			</div>
+			<div class="input flex flex-col gap-[1.28rem]">
+				<div class="email">
+					<Label for="email" class="block mb-2 text-[ 0.875rem]">Email</Label>
+					<Input
+						id="email"
+						size="lg"
+						placeholder="Enter your Email"
+						class="focus:border-2 focus:border-[#DA4E45] bg-white border-[#D9D9D9]"
+						type="email"
+					/>
+				</div>
+
+				<div class="password">
+					<Label for="password" class="block mb-2 text-[ 0.875rem]">Password</Label>
+					<Input
+						id="password"
+						size="lg"
+						placeholder="Enter your password"
+						class="focus:border-2 focus:border-[#DA4E45] bg-white border-[#D9D9D9]"
+						type="password"
+					/>
+				</div>
+			</div>
+			<div class="submit w-full flex flex-col gap-[1.88rem]">
+				<button class="bg-primary-50 py-[0.88rem] px-[0.63rem] rounded-[10px] w-full" type="submit">
+					<div class="button-text text-white font-bold text-sm">Log in</div>
+				</button>
+
+				<div class="forgot-password">
+					<div class="flex gap-[0.38rem] text-[0.8125rem] justify-center">
+						<div class="text-[#9C9C9C]">Forgotten password?</div>
+						<a href="/">Click here to reset</a>
+					</div>
+				</div>
+			</div>
+		</Card>
+	</form>
+</div>
