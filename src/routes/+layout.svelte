7b8bbfdb0494133b55a61ccdf1b590a476cbba1c
n3rd <godwinjdayo@gmail.com>
2023-11-18 21:38:38 +0100

    added fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,10 @@
-<script>import "../app.postcss";</script><slot></slot>
+<script>
+	import '../app.postcss';
+</script>
+
+<svelte:head>
+	<title>Spek-N-Boonen</title>
+	<link href="https://fonts.cdnfonts.com/css/inter" rel="stylesheet" />
+	<link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet" />
+</svelte:head>
+<slot />
