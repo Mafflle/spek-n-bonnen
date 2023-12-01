77db77441ac3c879c63f8600623d67b974d6f16f
n3rd <godwinjdayo@gmail.com>
2023-12-02 00:50:16 +0100

    📦 NEW: Staff Management page in progress

---
+++ src/lib/components/StaffMember.svelte
@@ -0,0 +1,49 @@
+<script>
+	/**
+	 * @type {String}
+	 */
+	export let name;
+	/**
+	 * @type {String}
+	 */
+	export let role;
+	/**
+	 * @type {String}
+	 */
+	export let email;
+	/**
+	 * @type {String}
+	 */
+	export let permissions;
+</script>
+
+<tr class="border-b-[0.5px] border-[#D9D9D9]">
+	<td>
+		<div class="  py-3 justify-start items-center gap-3 inline-flex">
+			<div class="w-[38px] h-[38px] bg-neutral-100 rounded-full"></div>
+			<div class="grow shrink basis-0 text-neutral-500 text-sm font-medium">
+				{name}
+			</div>
+		</div>
+	</td>
+	<td>
+		<div
+			class="w-[124px] h-6 px-1 py-[3px] bg-stone-50 rounded-[20px] justify-start items-center gap-2.5 inline-flex"
+		>
+			<div class="grow shrink basis-0 text-center text-neutral-500 text-[13px] font-medium">
+				{role}
+			</div>
+		</div>
+	</td>
+	<td>{email}</td>
+	<td>{permissions}</td>
+	<td
+		><div
+			class="w-7 h-[26px] px-1.5 py-3 origin-top-left -rotate-90 flex-col justify-center items-center gap-2.5 inline-flex"
+		>
+			<div class="justify-start items-start gap-[5px] inline-flex">
+				<img src="/icons/menu.svg" alt="" />
+			</div>
+		</div></td
+	>
+</tr>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -2,21 +2,25 @@
 	import NavbarButton from '$lib/components/NavBarButton.svelte';
 </script>
 
-<div class="min-w-screen min-h-screen inline-flex gap-[1.875rem] bg-[#F9F9F9] text-[#6B6B6B]">
+<div class="min-w-screen min-h-screen inline-flex gap-[1.875rem] bg-[#F9F9F9]">
 	<div class="side-nav w-[17.75rem] min-h-full flex flex-col">
 		<div
 			class="sidebar-logo inline-flex flex-col items-start gap-[0.25rem] px-[1.75rem] py-[2.44rem]"
 		>
 			<p class="text-center text-sm font-bold text-primary-50">Spek and Bonnen</p>
-			<sub class=" text-center text-[0.5rem] tracking-[0.125rem]">ERP SYSTEM</sub>
+			<sub class=" text-center text-[0.5rem] tracking-[0.125rem] text-[#6B6B6B]">ERP SYSTEM</sub>
 		</div>
-		<div class="sidebar-nav w-full flex p-[0.625rem] flex-col items-start gap-[0.625rem]">
+		<div
+			class="sidebar-nav w-full flex p-[0.625rem] flex-col items-start gap-[0.625rem] text-[#6B6B6B]"
+		>
 			<NavbarButton href="/" icon="/icons/home.svg" text="Homepage" />
 			<NavbarButton href="/" icon="/icons/shopping-cart.svg" text="Orders" alert={true} />
 			<NavbarButton href="/" icon="/icons/box.svg" text="Inventory" />
 			<NavbarButton href="/" icon="/icons/box.svg" text="Tasks" />
 		</div>
-		<div class="settings flex p-[0.625rem] flex-col justify-center items-start gap-[0.8125rem]">
+		<div
+			class="settings flex p-[0.625rem] flex-col justify-center items-start gap-[0.8125rem] text-[#6B6B6B]"
+		>
 			<div
 				class="settings-header text-secondary-50 text-[0.625rem] font-bold tracking-[0.0635rem] px-[1.75rem]"
 			>
