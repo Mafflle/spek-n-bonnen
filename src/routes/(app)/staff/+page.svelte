076dcdee10044966a706e454d67200485804e061
n3rd <godwinjdayo@gmail.com>
2023-12-03 07:15:05 +0100

    👌 IMPROVE: Improved staff search input

--- src/routes/(app)/staff/+page.svelte
+++ src/routes/(app)/staff/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import StaffMember from '$lib/components/StaffMember.svelte';
+	let isFocused = false;
 </script>
 
 <div class="staff-page flex-col items-start">
@@ -16,6 +17,32 @@
 				placeholder="Type here"
 				class="input input-bordered w-[22rem] py-[0.68rem] px-[0.37rem] rounded-[0.375rem] b-[#D9D9D9] border-[0.5px]"
 			/>
+		<div class="filters flex items-start gap-[26rem]">
+			<label class="relative text-gray-400 focus-within:text-gray-600 block">
+				{#if !isFocused}
+					<div
+						class="icon pointer-events-none w-8 h-8 absolute top-1/2 transform -translate-y-1/2 left-3 flex items-center"
+					>
+						<div class="w-[330px] h-6 justify-start items-center gap-[9px] inline-flex">
+							<div class="w-6 h-6 relative">
+								<img src="/icons/search.svg" alt="search icon" />
+							</div>
+							<div
+								class="grow shrink w-[inherit] basis-0 text-neutral-400 text-[13px] font-normal font-['Satoshi']"
+							>
+								Search for staffs...
+							</div>
+						</div>
+					</div>
+				{/if}
+				<input
+					type="text"
+					placeholder=""
+					class="input input-bordered w-[22rem] py-[0.68rem] px-[0.37rem] rounded-[0.375rem] b-[#D9D9D9] border-[0.5px]"
+					on:focus={() => (isFocused = true)}
+					on:blur={() => (isFocused = false)}
+				/>
+			</label>
 
 			<div class="filter-buttons flex items-start gap-5">
 				<button
