6ded5e2c8c26f26a61720348756c7a6b4da1223c
n3rd <godwinjdayo@gmail.com>
2023-12-04 15:31:43 +0100

    👌 IMPROVE: fix StaffMember component menu icon

--- src/lib/components/StaffMember.svelte
+++ src/lib/components/StaffMember.svelte
@@ -19,7 +19,7 @@
 
 <tr class="border-b-[0.5px] border-[#D9D9D9]">
 	<td>
-		<div class="  py-3 justify-start items-center gap-3 inline-flex">
+		<div class="py-3 justify-start items-center gap-3 inline-flex">
 			<div class="w-[38px] h-[38px] bg-neutral-100 rounded-full"></div>
 			<div class="grow shrink basis-0 text-[#6B6B6B] text-sm font-medium">
 				{name}
@@ -38,12 +38,6 @@
 	<td class="text-[#9C9C9C]">{email}</td>
 	<td class="text-[#9C9C9C]">{permissions}</td>
 	<td>
-		<div
-			class="w-7 h-[26px] px-1.5 py-3 origin-top-left -rotate-90 flex justify-center items-center gap-2.5"
-		>
-			<div class="justify-center items-center gap-[5px] inline-flex rotate-90">
-				<img src="/icons/menu.svg" alt="" />
-			</div>
-		</div>
+		<img src="/icons/menu.svg" alt="" class="self-center" />
 	</td>
 </tr>
