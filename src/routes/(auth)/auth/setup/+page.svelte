085c799c0910c6a12ab3074960fd3844b7fe5c8a
Harnassus <micfestus45@gmail.com>
2023-12-04 11:00:13 +0100

    IMPROVE: Fixed setup admin validation error styling issue

--- src/routes/(auth)/auth/setup/+page.svelte
+++ src/routes/(auth)/auth/setup/+page.svelte
@@ -9,20 +9,20 @@
 
 	let validationErrors: {
 		email?: [string];
-		firstName?: [string];
-		lastName?: [string];
+		first_name?: [string];
+		last_name?: [string];
 		password?: [string];
 		password2?: [string];
 	};
 
-	onMount(async () => {});
+	// onMount(async () => {});
 </script>
 
 <svelte:head>
-	<title>Setup - Spek-n-Boonen</title>
+	<title>Setup Admin - Spek-n-Boonen</title>
 </svelte:head>
 
-<div class="h-screen w-screen flex justify-center items-center bg-[#F2F2F2]">
+<div class="min-h-screen max-w-screen py-6 flex justify-center items-center bg-[#F2F2F2]">
 	<form
 		action="?/setup"
 		method="post"
@@ -51,26 +51,26 @@
 		}}
 	>
 		<div
-			class="bg-white shadow-none border-none rounded-[16px] py-[50px] px-[30px] text-[#2d2d2d] flex flex-col justify-center items-center gap-10 w-[22rem] lg:w-[28.25rem]"
+			class="bg-white shadow-none border-none rounded-2xl py-12 px-7 text-[#2d2d2d] flex flex-col justify-center items-center gap-10 min-w-[22rem] lg:min-w-[28.25rem]"
 		>
 			<div class="greeting w-full">
 				<div class="flex gap-[1.88rem] items-center w-full">
 					<div class="avatar w-auto h-16 flex items-center justify-center">
 						<img src="/icons/user.svg" alt="avatar" />
 					</div>
-					<div class="admin gap-2 w-auto h-16 justify-center">
+					<div class="admin gap-2 w-auto justify-center">
 						<h1 class="text-[1.1rem] lg:text-2xl font-medium tracking-[-0.03rem] whitespace-nowrap">
 							Setup admin account
 						</h1>
-						<sub class="text-[0.8rem] lg:text-[0.9375rem] tracking-[-0.00938rem] text-[#575757]">
-							Enter your credentials below</sub
-						>
+						<p class="text-[0.8rem] lg:text-[0.9375rem] tracking-[-0.00938rem] text-[#575757]">
+							Enter your credentials below
+						</p>
 					</div>
 				</div>
 			</div>
 			<div class=" flex flex-col gap-[1.28rem] w-full">
 				<div class="name flex flex-col md:flex-row gap-8">
-					<div class="first-name">
+					<div class="first-name flex flex-col">
 						<label for="first-name" class="block mb-2 text-[ 0.875rem]">First name</label>
 						<input
 							type="text"
@@ -80,15 +80,15 @@
 							class="input w-full md:w-[11.5rem] focus:border-1 focus:border-[#DA4E45] focus:shadow-custom border-[#D9D9D9] rounded-[0.5rem]"
 						/>
 
-						{#if validationErrors?.firstName}
+						{#if validationErrors?.first_name}
 							<sub
 								transition:slide={{ delay: 250, duration: 300 }}
 								class="text-rose-500 text-xs tracking-[-0.0075rem]"
-								>{validationErrors.firstName[0]}</sub
+								>{validationErrors.first_name[0]}</sub
 							>
 						{/if}
 					</div>
-					<div class="last-name">
+					<div class="last-name flex flex-col">
 						<label for="last-name" class="block mb-2 text-[ 0.875rem]">Last name</label>
 						<input
 							type="text"
@@ -97,16 +97,16 @@
 							placeholder="Last name"
 							class="input w-full md:w-[11.5rem] focus:border-1 focus:border-[#DA4E45] focus:shadow-custom border-[#D9D9D9] rounded-[0.5rem]"
 						/>
-						{#if validationErrors?.lastName}
+						{#if validationErrors?.last_name}
 							<sub
 								transition:slide={{ delay: 250, duration: 300 }}
 								class="text-rose-500 text-xs tracking-[-0.0075rem]"
-								>{validationErrors.lastName[0]}</sub
+								>{validationErrors.last_name[0]}</sub
 							>
 						{/if}
 					</div>
 				</div>
-				<div class="email">
+				<div class="email flex flex-col">
 					<label for="email" class="block mb-2 text-[ 0.875rem]">Email</label>
 					<input
 						type="email"
@@ -123,7 +123,7 @@
 					{/if}
 				</div>
 
-				<div class="password">
+				<div class="password flex flex-col">
 					<label for="password" class="block mb-2 text-[ 0.875rem]">Password</label>
 					<input
 						type="password"
@@ -140,7 +140,7 @@
 						>
 					{/if}
 				</div>
-				<div class="password">
+				<div class="password flex flex-col">
 					<label for="confirm-password" class="block mb-2 text-[ 0.875rem]">Confirm password</label>
 					<input
 						type="password"
