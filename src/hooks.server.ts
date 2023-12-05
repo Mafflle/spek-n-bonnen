6cebc8479e38e4bf992c3d95db1b1d5633a1fe8b
sheyzi <gistkiosk@gmail.com>
2023-12-05 03:41:28 +0100

    Fix origin issues

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,26 +1,18 @@
 import { PUBLIC_API_ENDPOINT } from '$env/static/public';
 
 export const handleFetch = async ({ request, fetch, event }) => {
-
 	if (request.url.startsWith(PUBLIC_API_ENDPOINT)) {
-	request.headers.set('Content-type', 'application/json');
-	request.headers.set('Accept', 'application/json');
-		request.headers.set('origin', event.url.origin)
-		// console.log(event.url.origin);
-		
-	const access = event.cookies.get('access');
-	if (access) {
-	// console.log('token is available')
-	request.headers.set('Authorization', `Bearer ${access}`);
-	
+		request.headers.set('Origin', event.url.origin);
+		request.headers.set('Content-Type', 'application/json');
+
+		const access = event.cookies.get('access');
+
+		if (access) {
+			request.headers.set('Authorization', `Bearer ${access}`);
+		}
 	}
-}
-
-
 
 	// console.log('hey');
-	
-	
+
 	return fetch(request);
 };
-
