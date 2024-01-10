ccd37e3147a743838ecfb5d12ffe71344dfb4ca2
n3-rd <godwinjdayo@gmail.com>
2024-01-10 08:03:15 +0100

    🐛 FIX: Update UploadBox component styles and fix multiple image uploads type error

--- src/lib/components/UploadBox.svelte
+++ src/lib/components/UploadBox.svelte
@@ -7,7 +7,7 @@
 
 	export let maximumImages: number = 10;
 	export let inputName: string = 'images';
-	export let allowMultiple: boolean = true;
+	export const allowMultiple: boolean = true;
 	const setPreview = (image: File) => {
 		previewImage = URL.createObjectURL(image);
 	};
@@ -59,7 +59,7 @@
 </script>
 
 <div
-	class="w-full relative rounded-xl flex py-8 px-24 flex-col items-start gap-3 self-stretch border border-grey-300 border-dashed"
+	class="w-full relative rounded-xl flex py-8 px-24 flex-col items-start gap-3 self-stretch border border-primary-50 border-dashed bg-[#FFF4F3]"
 >
 	<input
 		bind:files={images}
@@ -83,11 +83,26 @@
 				class=" h-full absolute w-full top-0 object-cover"
 			/>
 		{/if}
-		<div class="image w-14 h-14 rounded-full flex justify-center items-center bg-[#f5f5f5]">
+		<div
+			class="image w-14 h-14 rounded-full flex justify-center items-center border border-primary-softPink"
+		>
 			<img src="/icons/regular-image.svg" alt="reguar icon" />
 		</div>
-		<div class="text text-center text-sm leading-5 tracking-[-0.13px] text-[#CDCDCD]">
-			Drag and drop file or click <b class="text-primary-100">upload</b> from your computer
+		<div
+			class="text text-center text-sm leading-5 tracking-[-0.13px] text-black-100 flex flex-col items-center gap-3"
+		>
+			<div>Drag and drop file</div>
+			<div>or</div>
 		</div>
+		<button
+			class="bg-primary-50 py-[0.88rem] px-[0.63rem] w-52
+		hover:bg-[#C7453C] hover:rounded-[0.625rem]
+		focus:shadow-custom text-white font-bold text-sm max-h-12 flex items-center justify-center
+		rounded-3xl
+		"
+			type="submit"
+		>
+			<span class="button-text">Import from computer </span>
+		</button>
 	</div>
 </div>
