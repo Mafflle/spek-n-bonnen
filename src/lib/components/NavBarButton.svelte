2e0b13cf23300651f878acba24f349685b4b71c9
Harnassus <micfestus45@gmail.com>
2024-01-24 03:25:10 +0100

    IMPROVEMENT: Made Sidenav responsive

--- src/lib/components/NavBarButton.svelte
+++ src/lib/components/NavBarButton.svelte
@@ -2,6 +2,9 @@
 	import { page } from '$app/stores';
 	import * as Collapsible from '$lib/components/ui/collapsible';
 	import Button from './ui/button/button.svelte';
+	import * as Menubar from '$lib/components/ui/menubar';
+	import { SubTrigger } from './ui/dropdown-menu';
+	import { Menu } from 'lucide-svelte';
 
 	export let href: string = '';
 	export let icon: string = '';
@@ -16,10 +19,11 @@
 	const toggleChildren = () => {
 		showChildren = !showChildren;
 	};
+	console.log(children);
 </script>
 
 {#if children.length > 0}
-	<Collapsible.Root class="w-full h-full">
+	<Collapsible.Root class="md:w-full h-full hidden md:flex">
 		<a
 			on:mouseenter={() => {
 				if ($page.url.pathname === href && active === true) active = false;
@@ -27,7 +31,7 @@
 			on:mouseleave={() => {
 				if ($page.url.pathname === href && active === false) active = true;
 			}}
-			class="flex max-w-full w-full h-full px-3 items-center justify-around rounded-md
+			class="flex max-w-full md:w-full h-full px-3 items-center justify-around rounded-md
 		hover:bg-grey-200 hover:text-white hover:shadow-inner {active && 'bg-primary-light text-[#DA4E45]'}
 		"
 			{href}
@@ -35,7 +39,9 @@
 			<span class="side-nav-button w-full flex items-center justify-between">
 				<div class="button-content flex items-center gap-2.5 self-stretch">
 					<span>{@html active ? activeIcon : icon}</span>
-					<span class="button-text flex flex-col justify-center flex-shrink-0 self-stretch text-sm">
+					<span
+						class="button-text hidden md:flex flex-col justify-center flex-shrink-0 self-stretch text-sm"
+					>
 						{text}
 					</span>
 				</div>
@@ -50,14 +56,14 @@
 				{/if}
 			</span>
 
-			<Collapsible.Trigger asChild let:builder>
+			<!-- <Collapsible.Trigger class="h" asChild let:builder>
 				<Button
 					builders={[builder]}
-					class="flex items-center justify-center p-0 {active && 'text-black-100'}"
+					class="hidden md:flex items-center justify-center p-0 {active && 'text-black-100'}"
 					><iconify-icon icon="iconamoon:arrow-up-2-fill" width="20" rotate="90deg"></iconify-icon>
 					<span class="sr-only">Toggle</span></Button
 				>
-			</Collapsible.Trigger>
+			</Collapsible.Trigger> -->
 		</a>
 
 		<Collapsible.Content class="py-5">
@@ -78,10 +84,99 @@
 			</ul>
 		</Collapsible.Content>
 	</Collapsible.Root>
+	<Menubar.Root
+		class="md:w-full h-full flex p-2 justify-center  bg-transparent {active &&
+			'bg-primary-light text-[#DA4E45]'} rounded-full	border-0 space-x-0 w-full md:hidden"
+	>
+		<Menubar.Menu>
+			<Menubar.Trigger class="p-0">
+				<a
+					on:mouseenter={() => {
+						if ($page.url.pathname === href && active === true) active = false;
+					}}
+					on:mouseleave={() => {
+						if ($page.url.pathname === href && active === false) active = true;
+					}}
+					class="flex max-w-full md:w-full h-full items-center justify-around rounded-md
+	md:hover:bg-grey-200 md:hover:text-white md:hover:shadow-inner {active &&
+						'bg-primary-light text-[#DA4E45]'}
+	"
+					{href}
+				>
+					<span class="side-nav-button w-full flex items-center justify-between">
+						<div class="button-content flex items-center gap-2.5 self-stretch">
+							<span>{@html active ? activeIcon : icon}</span>
+							<span
+								class="button-text hidden md:flex flex-col justify-center flex-shrink-0 self-stretch text-sm"
+							>
+								{text}
+							</span>
+						</div>
+						{#if alert}
+							<div
+								class="button-alert flex w-[1.875rem] py-[0.1875rem] px-[0.625rem] flex-col justify-center items-center gap-[0.625rem] self-stretch
+	rounded-[1.875rem] bg-[#FF1C0D]
+	"
+							>
+								<span class="alert-text text-white text-xs font-bold">4</span>
+							</div>
+						{/if}
+					</span>
+
+					<!-- <Menubar.Trigger class="h" asChild let:builder>
+			<Button
+				builders={[builder]}
+				class="hidden md:flex items-center justify-center p-0 {active && 'text-black-100'}"
+				><iconify-icon icon="iconamoon:arrow-up-2-fill" width="20" rotate="90deg"></iconify-icon>
+				<span class="sr-only">Toggle</span></Button
+			>
+		</Menubar.Trigger> -->
+				</a>
+			</Menubar.Trigger>
+
+			<Menubar.Content class="bg-black-100 text-white">
+				{#each children as child}
+					{#if child.childRoutes}
+						<Menubar.Sub>
+							<Menubar.SubTrigger class="text-xs font-semibold">
+								<span class="p-2">{child.title}</span>
+							</Menubar.SubTrigger>
+							<Menubar.SubContent
+								class="flex flex-col gap-3 justify-between bg-black-100 text-white"
+							>
+								{#each child.childRoutes as route}
+									<Menubar.Item>
+										<a
+											class="p-2 {childActive && 'bg-primary-light text-[#DA4E45] shadow-inner'}
+												  rounded-md flex gap-3 active hover:bg-primary-light hover:text-primary-red"
+											href={route.href}
+										>
+											<div class="item-title font-medium text-xs">{route.title}</div>
+										</a>
+									</Menubar.Item>
+								{/each}
+							</Menubar.SubContent>
+						</Menubar.Sub>
+					{:else}
+						<Menubar.Item>
+							<a
+								class="p-2 w-full {childActive && 'bg-primary-light text-[#DA4E45] shadow-inner'}
+												  rounded-md flex gap-3 active hover:bg-primary-light hover:text-primary-red"
+								href={child.href}
+							>
+								<div class="item-title font-medium text-xs">{child.title}</div>
+							</a>
+						</Menubar.Item>
+					{/if}
+				{/each}
+			</Menubar.Content>
+		</Menubar.Menu>
+	</Menubar.Root>
 {:else}
 	<a
-		class="flex max-w-full h-full py-3 px-3 items-center justify-around rounded-md
-										hover:bg-grey-200 hover:text-white hover:shadow-inner {active && 'bg-primary-light text-[#DA4E45]'}
+		class="flex max-w-full w-full md:h-full md:border-0 relative p-2 md:py-3 md:px-3 items-center justify-around rounded-full md:rounded-md
+										md:hover:bg-grey-200 hover:text-white hover:shadow-inner {active &&
+			' bg-primary-light text-[#DA4E45]'}
 										"
 		{href}
 		on:mouseenter={() => {
@@ -92,20 +187,24 @@
 		}}
 	>
 		<span class="side-nav-button flex max-w-full w-full items-center justify-between">
-			<div class="button-content flex items-center w-full gap-2.5 self-stretch">
+			<div
+				class="button-content flex items-center justify-center w-full md:gap-2.5 md:self-stretch"
+			>
 				<!-- <enhanced:img src={icon} /> -->
-				<span>{@html active ? activeIcon : icon}</span>
-				<span class="button-text flex flex-col justify-center flex-shrink-0 self-stretch text-sm">
+				<span class="">{@html active ? activeIcon : icon}</span>
+				<span
+					class="button-text hidden md:flex flex-col justify-center flex-shrink-0 self-stretch text-sm"
+				>
 					{text}
 				</span>
 			</div>
 			{#if alert}
 				<div
-					class="button-alert flex w-[1.875rem] py-[0.1875rem] px-[0.625rem] flex-col justify-center items-center gap-[0.625rem] self-stretch
-						rounded-[1.875rem] bg-[#FF1C0D]
+					class="button-alert absolute md:static -top-1 -right-1 flex w-[1.875rem] py-[0.1875rem] px-[0.625rem] flex-col justify-center items-center gap-[0.625rem] self-stretch
+						md:rounded-[1.875rem] md:bg-[#FF1C0D]
 						"
 				>
-					<span class="alert-text text-white text-xs font-bold">4</span>
+					<span class="alert-text text-primary-red text-xs font-bold">4</span>
 				</div>
 			{/if}
 		</span>
--- src/lib/components/SideNav.svelte
+++ src/lib/components/SideNav.svelte
@@ -31,19 +31,24 @@
 			pageTitle: 'Inventory',
 			children: [
 				{
-					title: 'Brands',
-					href: '/inventory/brands',
-					color: '#CFB53B'
-				},
-				{
-					title: 'Vendors',
-					href: '/inventory/vendors',
-					color: '#41AA00'
-				},
-				{
-					title: 'Manufacturers',
-					href: '/inventory/manufacturers',
-					color: '#FF1C0D'
+					title: 'Providers',
+					childRoutes: [
+						{
+							title: 'Brands',
+							href: '/inventory/brands',
+							color: '#CFB53B'
+						},
+						{
+							title: 'Vendors',
+							href: '/inventory/vendors',
+							color: '#41AA00'
+						},
+						{
+							title: 'Manufacturers',
+							href: '/inventory/manufacturers',
+							color: '#FF1C0D'
+						}
+					]
 				},
 				{
 					title: 'Carcasses',
@@ -90,7 +95,7 @@
 </script>
 
 <aside
-	class="side-nav w-[18rem] sticky top-0 h-screen overflow-scroll no-scrollbar px-4 flex flex-col gap-5 bg-black-100 text-white"
+	class="side-nav w-[18rem] sticky top-0 h-screen overflow-scroll no-scrollbar px-4 hidden md:flex flex-col gap-5 bg-black-100 text-white"
 >
 	<div class="sidebar-logo flex flex-col items-start mb-10 gap-3 px-3 pt-10">
 		<h3 class="text-center text-sm font-bold text-primary-50">Spek and Bonnen</h3>
@@ -132,3 +137,37 @@
 		</ol>
 	</nav>
 </aside>
+
+<nav class="w-screen flex md:hidden items-center justify-center xs:fixed bottom-4 right-0">
+	<ol
+		class="w-[85%] h-10 bg-black-100 z-10 flex items-center justify-between px-3 py-6 rounded-full shadow-md"
+	>
+		{#each routes as route}
+			{#if route.pageTitle !== 'Orders'}
+				<li class="w-full">
+					<NavBarButton
+						active={routes.indexOf(route) !== 0
+							? $page.url.pathname.startsWith(route.href)
+							: $page.url.pathname === route.href}
+						icon={route.icon}
+						href={route.href}
+						activeIcon={route.activeIcon}
+						text={route.pageTitle}
+						children={route?.children}
+					/>
+				</li>
+			{:else if route.pageTitle === 'Orders'}
+				<li class="w-full">
+					<NavBarButton
+						active={$page.url.pathname.startsWith(route.href)}
+						icon={route.icon}
+						href={route.href}
+						activeIcon={route.activeIcon}
+						text={route.pageTitle}
+						alert={true}
+					/>
+				</li>
+			{/if}
+		{/each}
+	</ol>
+</nav>
