a50202a6fa7991da4d3ba8479402e3d5eb13a65c
n3-rd <godwinjdayo@gmail.com>
2024-02-05 14:48:56 +0100

    added custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<main class="flex flex-col items-center justify-center h-screen text-center">
+	<h1 class="text-2xl font-bold">Error: {$page.error?.message}</h1>
+	<h2 class="text-xl">Error Code: {$page.status}</h2>
+	<a href="/" class="text-primary-100">Go back home</a>
+</main>
