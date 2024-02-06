a91ece993ee780d2d7b004c35bd00a6d9cc31256
n3-rd <godwinjdayo@gmail.com>
2024-02-06 14:05:16 +0100

    👌 IMPROVE: Added Carcass table

--- src/routes/(app)/inventory/carcass/+page.svelte
+++ src/routes/(app)/inventory/carcass/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { Carcasses } from '$lib/stores/carcass.stores';
-
+	import Carcass from '$lib/components/Carcass.svelte';
 	export let data;
-	console.log(data);
-
-	Carcasses.set(data.carcasses);
+	console.log('paged', data.carcasses);
+	let grid = false;
+	Carcasses.set(data.carcasses.results);
 	$: console.log($Carcasses);
 </script>
 
@@ -63,3 +63,67 @@
 		</div>
 	</div>
 </div>
+<!-- render if page is empty -->
+{#if $Carcasses.length === 0}
+	<div class="empty h-full w-full flex justify-center items-center">
+		<div class="empty-indicator flex flex-col justify-center items-center gap-5 w-[277px]">
+			<div class="icon">
+				<img src="/icons/empty-Illustration.svg" alt="empty illustration " />
+			</div>
+			<div class="content">
+				<div class="text flex flex-col gap-3 justify-center items-center text-grey-300">
+					<h3 class="title text-3xl font-medium tracking-[-0.64px]">No carcass added</h3>
+					<div class="info text-center text-sm font-medium leading-5 tracking-[-0.13px]">
+						You currently don’t have any carcass stored in your inventory, click the button below to
+						add one
+					</div>
+				</div>
+			</div>
+			<div class="button">
+				<a
+					href="carcass/manage"
+					class="px-3 py-2 bg-primary-100 text-sm text-white rounded-md flex items-center gap-1"
+				>
+					<img src="/icons/plus.svg" alt="Plus icon to represent adding" />
+					<span class="hidden sm:block">Add carcass</span>
+				</a>
+			</div>
+		</div>
+	</div>
+{/if}
+{#if grid}
+	<!-- Check if grid is false -->
+	<div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+		{#each $Carcasses as carcass (carcass?.id)}
+			{#if carcass}
+				<Carcass {carcass} {grid} />
+			{/if}
+		{/each}
+	</div>
+{:else}
+	<!-- If grid is true, render the table -->
+	<div class="border rounded-xl">
+		<table class="table">
+			<thead>
+				<tr class="">
+					<th class="bg-[#F9F9F9] rounded-tl-[0.625rem]">Name</th>
+					<th class="bg-[#F9F9F9]">Vendor</th>
+					<th class="bg-[#F9F9F9]">Weight</th>
+					<th class="bg-[#F9F9F9]">Purchase price</th>
+					<th class="bg-[#F9F9F9]">ABHD code</th>
+					<th class="bg-[#F9F9F9]">Date Recieved</th>
+					<th class="bg-[#F9F9F9]">Fat Score</th>
+					<th class="bg-[#F9F9F9] rounded-tr-[0.625rem]"></th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{#each $Carcasses as carcass (carcass?.id)}
+					{#if carcass}
+						<Carcass {carcass} {grid} />
+					{/if}
+				{/each}
+			</tbody>
+		</table>
+	</div>
+{/if}
