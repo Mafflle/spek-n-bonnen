e19faa3cd2a2b9826e689f689a09d17c36ef818e
n3-rd <godwinjdayo@gmail.com>
2024-02-13 16:11:18 +0100

    fixed chrome autofill bug

--- src/routes/(app)/inventory/carcass/+page.svelte
+++ src/routes/(app)/inventory/carcass/+page.svelte
@@ -69,12 +69,44 @@
 			<div
 				class="flex items-center sm:w-[24em] border gap-2 rounded-md border-[#D9D9D9] text-[#232222] px-2"
 			>
-				<form method="post" action="?/search" use:enhance={searchCarcass} bind:this={form}>
+				<span>
+					<svg
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						xmlns="http://www.w3.org/2000/svg"
+					>
+						<path
+							d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
+							stroke="#A9A9A9"
+							stroke-width="1.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+						<path
+							d="M21 20.9999L16.65 16.6499"
+							stroke="#A9A9A9"
+							stroke-width="1.5"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+						/>
+					</svg>
+				</span>
+				<form
+					method="post"
+					action="?/search"
+					use:enhance={searchCarcass}
+					bind:this={form}
+					name="search"
+					id="search-carcass"
+					class="w-full"
+				>
 					<input type="submit" class="hidden" value="submit" />
 					<input
 						type="text"
 						placeholder="Search for carcass..."
-						class="py-2 flex-auto outline-none"
+						class="py-2 flex-auto outline-none w-full"
 						name="search"
 						on:input={() => {
 							submitSearch();
