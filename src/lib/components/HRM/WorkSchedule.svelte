2ee1cc47898ece5792cf6bd2c7c14846a84f425e
Harnassus <micfestus45@gmail.com>
2024-06-16 11:04:51 +0100

    Finished basic readonly scheduling UI for staffs

--- src/lib/components/HRM/WorkSchedule.svelte
+++ src/lib/components/HRM/WorkSchedule.svelte
@@ -6,26 +6,28 @@
 
 	interface WorkDay {
 		day: string;
-		shift: ShiftType;
+		id: number;
 		clockInTime: string | null;
 		clockOutTime: string | null;
 	}
 
 	const workSchedule: WorkDay[] = [
-		{ day: 'Monday', shift: 'Morning', clockInTime: '09:00 AM', clockOutTime: '05:00 PM' },
-		{ day: 'Tuesday', shift: 'Afternoon', clockInTime: '01:00 PM', clockOutTime: '09:00 PM' },
-		{ day: 'Wednesday', shift: 'Morning', clockInTime: '09:00 AM', clockOutTime: '05:00 PM' },
-		{ day: 'Thursday', shift: 'Afternoon', clockInTime: '01:00 PM', clockOutTime: '09:00 PM' },
-		{ day: 'Friday', shift: 'Morning', clockInTime: '09:00 AM', clockOutTime: '05:00 PM' }
-		// { day: 'Saturday', shift: 'Afternoon', clockInTime: '01:00 PM', clockOutTime: '09:00 PM' },
-		// { day: 'Sunday', shift: 'Off', clockInTime: null, clockOutTime: null } // Day off
+		{ id: 1, day: 'Monday', clockInTime: '09:00 AM', clockOutTime: '05:00 PM' },
+		{ id: 2, day: 'Tuesday', clockInTime: '01:00 PM', clockOutTime: '09:00 PM' },
+		{ id: 3, day: 'Wednesday', clockInTime: '09:00 AM', clockOutTime: '05:00 PM' },
+		{ id: 4, day: 'Thursday', clockInTime: '01:00 PM', clockOutTime: '09:00 PM' },
+		{ id: 5, day: 'Friday', clockInTime: '09:00 AM', clockOutTime: '05:00 PM' },
+		{ id: 6, day: 'Saturday', clockInTime: '01:00 PM', clockOutTime: '09:00 PM' },
+		{ id: 0, day: 'Sunday', clockInTime: null, clockOutTime: null }
 	];
+
+	let today = new Date();
 </script>
 
 <div class="w-full h-fit overflow-clip border border-grey-300 mt-8 rounded-xl">
-	<Table.Root class="px-2 gap-5">
+	<Table.Root class="">
 		<Table.Caption>This week's schedule.</Table.Caption>
-		<Table.Header class="">
+		<Table.Header class="sticky top-0">
 			<Table.Row class="h-[55px] bg-[#F9F9F9] font-satoshi font-medium ">
 				<Table.Head class=" text-grey-200 ">Day</Table.Head>
 				<!-- <Table.Head class="text-grey-200">Shift</Table.Head> -->
@@ -36,7 +38,10 @@
 		</Table.Header>
 		<Table.Body>
 			{#each workSchedule as schedule, i (i)}
-				<Table.Row class="h-[100px] ">
+				<Table.Row
+					class="h-[100px] {!(today.getDay() === schedule.id) &&
+						'opacity-40 cursor-not-allowed bg-gray-50'}"
+				>
 					<Table.Cell class="font-medium  text-grey-100 ">{schedule.day}</Table.Cell>
 
 					<Table.Cell class="font-medium  text-grey-100 text-center"
@@ -46,7 +51,7 @@
 						>{schedule.clockOutTime}</Table.Cell
 					>
 					<Table.Cell class="font-medium  text-grey-100">
-						<ClockInOut />
+						<ClockInOut disabled={!(today.getDay() === schedule.id)} />
 					</Table.Cell>
 				</Table.Row>
 			{/each}
