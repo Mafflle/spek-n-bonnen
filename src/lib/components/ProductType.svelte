5b5be844467813ac382e4b57e892a69d072f01b7
n3-rd <godwinjdayo@gmail.com>
2024-08-07 23:45:21 +0000

    created product type ui

---
+++ src/lib/components/ProductType.svelte
@@ -0,0 +1,84 @@
+<script>
+	import {
+		Calendar,
+		CaseLower,
+		Check,
+		ChevronDown,
+		CircleDot,
+		FileDigit,
+		LucideCalendarClock,
+		ToggleRight,
+		Trash2,
+		PlusCircleIcon
+	} from 'lucide-svelte';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import { Button } from '$lib/components/ui/button';
+	import { Switch } from '$lib/components/ui/switch';
+	import { Separator } from '$lib/components/ui/separator';
+	const dataTypes = [
+		{ name: 'Number', icon: FileDigit },
+		{ name: 'String', icon: CaseLower },
+		{ name: 'Boolean', icon: ToggleRight },
+		{ name: 'Datetime', icon: LucideCalendarClock },
+		{ name: 'Date', icon: Calendar },
+		{ name: 'Checkbox', icon: Check },
+		{ name: 'Multiple Choice', icon: CircleDot }
+	];
+</script>
+
+<div class="flex flex-col items-start gap-6 w-[523px] type__container">
+	<div
+		class="type__inputs flex flex-col justify-center items-end p-5 gap-4 w-full border-l-4 border-l-yellow-300 rounded-sm"
+	>
+		<div class="type__dropdowns flex items-start gap-3 w-full">
+			<div class="name flex flex-col gap-2 w-1/2">
+				<div class="type__name text-sm leading-5 text-black-100 font-medium">Name</div>
+				<input
+					class="type__input w-full flex items-center py-2 h-[46px] px-2 border border-grey-300 gap-2 rounded-lg
+                    outline-none focus:outline-primary-100 focus:border-primary-100 placeholder:text-sm placeholder:font-satoshi
+                    "
+				/>
+			</div>
+			<div class="dropdown__container flex flex-col gap-2 w-1/2">
+				<div class="dropdown__name text-sm leading-5 text-black-100 font-medium">Data type</div>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger>
+						<div
+							class="dropdown__trigger w-full flex items-center py-3 px-2 border border-grey-300 gap-2 rounded-lg"
+						>
+							<div class="dropdown__placeholder w-[219px] h-5 text-left">Test</div>
+							<ChevronDown size="20" />
+						</div>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content align="end">
+						<DropdownMenu.Group>
+							{#each dataTypes as type}
+								<DropdownMenu.Item class="flex py-2 cursor-pointer px-3 gap-2 rounded-md">
+									<svelte:component this={type.icon} size="18" />
+									<span class="text-sm leading-4 text-black-100">{type.name}</span>
+								</DropdownMenu.Item>
+							{/each}
+						</DropdownMenu.Group>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+			</div>
+		</div>
+
+		<Separator />
+		<div class="type__options flex gap-6 max-h-5 text-grey-200 items-center">
+			<Button variant="ghost">
+				<Trash2 />
+			</Button>
+			<Separator orientation="vertical" />
+			<div class="toggle-required flex gap-2 items-center">
+				<span class="text-sm leading-5 text-black-100">Required</span>
+				<Switch class="bg-primary-100" />
+			</div>
+		</div>
+	</div>
+
+	<Button class="create-new flex items-center gap-2 text-grey-200 p-0 hover:text-primary-100">
+		<PlusCircleIcon />
+		<span class="text-sm leading-5">Create new</span>
+	</Button>
+</div>
--- src/lib/components/customs/DataType.form.svelte
+++ src/lib/components/customs/DataType.form.svelte
@@ -1,52 +1,24 @@
 <script lang="ts">
 	import * as Select from '$lib/components/ui/select/index.js';
+	import ProductType from '../ProductType.svelte';
+	import { Button } from '$lib/components/ui/button';
 
 	let dataTypes = [{}];
 </script>
 
-<div
-	class="min-h-[160px] w-[500px] h-full bg-white border-l-4 border-yellow-300 rounded shadow flex items-center gap-2["
->
-	<div class="form-group w-full">
-		<label for="name" class="font-satoshi text-sm font-medium mb-1">Name</label>
-		<input
-			type="text"
-			name="name"
-			id="name"
-			placeholder="Enter name"
-			class="input w-full focus:border-1 focus:border-[#DA4E45] focus:shadow-custom border-[#D9D9D9] rounded-[0.5rem]"
-		/>
+<div class="w-full h-full flex justify-center">
+	<form
+		method=""
+		action=""
+		class="flex flex-col justify-between items-end gap-6 w-[523px] min-h-full"
+	>
+		<ProductType />
 
-		<div class="form-item w-full flex flex-col gap-1">
-			<label for="priority" class="text-sm mb-1 font-medium font-satoshi">Priority</label>
-
-			<!-- <Select.Root bind:selected={taskPriority}>
-				<Select.Trigger class="w-full flex items-center">
-					<Select.Value placeholder="Select task priority" />
-				</Select.Trigger>
-				<Select.Content>
-					<Select.Group>
-						{#each taskPriorities as priority}
-							<Select.Item
-								class="flex items-center gap-3 justify-start"
-								value={priority.value}
-								label={priority.label}
-							>
-								<iconify-icon class={priority.color} width="20" icon="material-symbols:flag-outline"
-								></iconify-icon>
-								<span>{priority.label}</span>
-							</Select.Item>
-						{/each}
-					</Select.Group>
-				</Select.Content>
-				<Select.Input name="priority" />
-			</Select.Root>
-			{#if validationErrors?.priority}
-				<sub
-					transition:slide={{ delay: 250, duration: 300 }}
-					class="text-rose-500 text-xs tracking-[-0.0075rem]">{validationErrors.priority[0]}</sub
-				>
-			{/if} -->
-		</div>
-	</div>
+		<Button
+			type="submit"
+			class="flex  bg-primary-red gap-2 items-center font-satoshi text-sm font-bold text-white py-2.5 px-3 rounded-md hover:bg-primary-100"
+		>
+			Create product type
+		</Button>
+	</form>
 </div>
