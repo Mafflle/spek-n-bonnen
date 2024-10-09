f6600d6a8bf7d61844534948df095ac62c841257
Hael <micfestus45@gmail.com>
2024-10-09 11:03:00 +0100

    Fix: active state for sidebar link now works

---
+++ src/components/app/Link.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let route: { link: string; name: string; icon?: string; activeIcon?: string };
+
+	$: active = $page.url.pathname === route.link;
+</script>
+
+<a
+	class="flex h-full w-full items-center gap-2 rounded px-2 py-1 text-grey-100 hover:bg-black {active &&
+		'bg-black text-white '}"
+	href={route.link}
+>
+	{#if route.icon}
+		<span class="h-5 w-5">
+			<img src={active ? route.activeIcon : route.icon} class="h-full w-full" alt="" />
+		</span>
+	{/if}
+
+	<span class=" font-medium">{route.name}</span>
+</a>
--- src/components/app/SidenavLink.svelte
+++ src/components/app/SidenavLink.svelte
@@ -4,13 +4,15 @@
 	import Dropdown from '$components/Dropdown.svelte';
 	import DropdownContent from '$components/DropdownContent.svelte';
 
+	import Link from './Link.svelte';
+
 	export let route: SidenavLink;
 
 	interface SidenavLink {
 		name: string;
-		icon: string;
+		icon?: string;
 		link: string;
-		activeIcon: string;
+		activeIcon?: string;
 		children?: SidenavLink[];
 	}
 
@@ -19,10 +21,17 @@
 
 	$: if ($page.url.pathname === '/' && route.link === '/') {
 		isActive = true;
-	} else if (route.link === $page.url.pathname) {
+	} else if ($page.url.pathname.startsWith(route.link) && route.link !== '/') {
 		isActive = true;
+		if (route.children) {
+			showChildren = true;
+		}
 	} else {
 		isActive = false;
+	}
+
+	if (!isActive) {
+		showChildren = false;
 	}
 </script>
 
@@ -34,7 +43,7 @@
 	>
 		<li
 			class:active-link={isActive}
-			class=" flex w-full rounded hover:bg-black"
+			class=" flex w-full rounded text-grey-100 hover:bg-black"
 			slot="dropdown-trigger"
 		>
 			<span class="flex h-full w-full items-center gap-2 rounded px-2 py-1.5">
@@ -45,7 +54,7 @@
 						alt="{route.name} icon"
 					/>
 				</span>
-				<span class=" text-sm font-medium text-grey-100">{route.name}</span>
+				<span class=" text-sm font-medium">{route.name}</span>
 			</span>
 			<span class="flex h-7 w-7" class:rotate={showChildren}>
 				<img src="/icons/caret-down.svg" class="h-full w-full" alt="Caret down icon" />
@@ -55,15 +64,10 @@
 		<svelte:fragment slot="dropdown-content">
 			<DropdownContent bind:isOpen={showChildren}>
 				<svelte:fragment slot="dropdown-items">
-					<ul class="children-container relative mt-2 grid grid-cols-1">
+					<ul class="children-container relative mt-2 grid grid-cols-1 gap-1">
 						{#each route.children as child}
-							<li
-								class:active-link={isActive}
-								class="relative flex items-start rounded px-2 py-1 hover:bg-black"
-							>
-								<a href={child.link}>
-									<span class="text-xs font-medium text-grey-100">{child.name}</span>
-								</a>
+							<li class="relative flex items-start text-xs">
+								<Link route={child} />
 							</li>
 						{/each}
 					</ul>
@@ -72,13 +76,15 @@
 		</svelte:fragment>
 	</Dropdown>
 {:else}
-	<li class:active-link={isActive} class="h-full w-full rounded text-grey-100 hover:bg-black">
-		<a class="flex h-full w-full items-center gap-2 rounded px-2 py-1.5" href={route.link}>
+	<li class:active-link={isActive} class="flex items-center text-sm">
+		<Link {route} />
+		<!-- <a class="flex h-full w-full items-center gap-2 rounded px-2 py-1.5 text-grey-100" href={route.link}>
 			<span class="h-5 w-5">
-				<img src={isActive ? route.activeIcon : route.icon} class="h-full w-full" alt="home icon" />
+				<img src={isActive ? route.activeIcon : route.icon} class="h-full w-full" alt="" />
 			</span>
+
 			<span class=" text-sm font-medium">{route.name}</span>
-		</a>
+		</a> -->
 	</li>
 {/if}
 
